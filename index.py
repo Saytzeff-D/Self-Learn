6117f1d8d05333c99748172defc0c83a43f5d1cd
David Atanda <ololadedavid15@gmail.com>
2023-08-08 10:15:26 +0100

    variable declaration and concatenation

---
+++ index.py
@@ -0,0 +1,16 @@
+print('Hello World')
+
+stud_name = 'David'
+address = 'Ogbomoso'
+score = '94'
+age = 12
+
+print('My name is',stud_name,"I live in",address, "and I am", age, "years old")
+
+first_name, last_name, age = 'David', 'Atanda', 12
+x = y = z = 'Sugar'
+
+print(f"My name is {first_name} {last_name}. I am {age} years old")
+
+# Various types of variables, types of concatenation - Assignment
+# Write a comprehensive note about yourself
